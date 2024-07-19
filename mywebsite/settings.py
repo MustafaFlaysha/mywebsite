fc2291ad9d9b026f51175433174d43c520e4367c
MustafaFlaysha <mustafa.elbizanti@gmail.com>
2024-07-19 09:11:01 +0200

    Update settings.py

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,18 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_DJANGO_KEY')
+SECRET_KEY = 'Sg5=!;%V\lpN*\,dvn'"68h=W!:B""PtFLj=PMhL'fDbI!60.2)'
 
-if not SECRET_KEY:
-    raise ValueError("The SECRET_DJANGO_KEY environment variable is not set.")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = [*]
 
-if not ALLOWED_HOSTS:
-    raise ValueError("The DJANGO_ALLOWED_HOSTS environment variable is not set.")
+
 
 
 # Application definition
@@ -123,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
